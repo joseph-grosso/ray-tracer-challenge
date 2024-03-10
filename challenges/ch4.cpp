ee28fe16720b81f28e105cfac60e638486ce044e
joegr <joe.grosso@cogeco.ca>
2024-03-10 16:10:10 -0400

    Add outputs to git repo

--- challenges/ch4.cpp
+++ challenges/ch4.cpp
@@ -28,28 +28,82 @@ int main() {
     float pi = 3.14159;
     Tuple pos = point(1, 0, 0);
     Tuple canvas_pos;
+    Tuple reflected_canvas_pos;
 
-    Matrix one_clock_turn = rotation_z_matrix(pi / 6);
-    Matrix map_to_canvas = translation_matrix(50, 50, 0) * scaling_matrix(40, 40, 0);
+    Matrix one_clock_turn = rotation_z_matrix(- pi / 100);
+    Matrix map_to_canvas = translation_matrix(30, 50, 0) * scaling_matrix(20, 20, 0);
 
     Canvas c(100, 100);
 
     std::cout << std::setprecision(2) << std::fixed;
     std::cout << "New clock starting!" << std::endl;
 
-    for (int i=0; i < 12; i++) {
+    // top
+    for (int i=0; i <= 100; i++) {
         // Remap current point to canvas_pos
         canvas_pos = map_to_canvas * pos;
+        reflected_canvas_pos = translation_matrix(40, 0, 0) * canvas_pos;
         // Fill in canvas at the canvas coords
-        c.write_pixel(Color(1, 1, 1), canvas_pos.x, canvas_pos.y);
+        c.write_pixel(Color(1, 0, 0), canvas_pos.x, canvas_pos.y);
+        c.write_pixel(Color(1, 0, 0), reflected_canvas_pos.x, reflected_canvas_pos.y);
         // Rotate base position
         pos = one_clock_turn * pos;
     };
 
-    std::string filename = "clock_value.ppm";
+    // bottom left
+    Tuple line_pos = point(0, 0, 0);
+    int movement_factor = 10;
+    map_to_canvas = translation_matrix(10, 50, 0);
+    Matrix movement = translation_matrix(1, 1, 0);
+    for (int i=0; i <= 40; i++) {
+        // Remap current point to canvas_pos
+        canvas_pos = map_to_canvas * line_pos;
+        reflected_canvas_pos = translation_matrix(40, 0, 0) * canvas_pos;
+        // Fill in canvas at the canvas coords
+        c.write_pixel(Color(1, 0, 0), canvas_pos.x, canvas_pos.y);
+        // Move base position
+        line_pos = movement * line_pos;
+    };
+
+    // bottom right
+    line_pos = point(0, 0, 0);
+    map_to_canvas = translation_matrix(89, 50, 0);
+    movement = translation_matrix(-1, 1, 0);
+    for (int i=0; i <= 40; i++) {
+        // Remap current point to canvas_pos
+        canvas_pos = map_to_canvas * line_pos;
+        reflected_canvas_pos = translation_matrix(40, 0, 0) * canvas_pos;
+        // Fill in canvas at the canvas coords
+        c.write_pixel(Color(1, 0, 0), canvas_pos.x, canvas_pos.y);
+        // Move base position
+        line_pos = movement * line_pos;
+    };
+    std::string filename = "heart.ppm";
     c.write_to_ppm(filename);
 
     std::cout << "Charted at " << filename << std::endl;
 
     return 0;
 }
+
+
+    // float pi = 3.14159;
+    // Canvas c(100, 100);
+
+    // std::cout << std::setprecision(2) << std::fixed;
+    // std::cout << "New clock starting!" << std::endl;
+
+    // Tuple canvas_pos;
+
+    // // top
+    // Tuple pos_circles = point(1, 0, 0);
+    // Matrix scale = translation_matrix(20, -50, 0) * scaling_matrix(20, 20, 0);
+    // Matrix one_of_10_rotations = rotation_z_matrix(1 / 10);
+    // for (int i=0; i < 10; i++) {
+    //     // Remap current point to canvas_pos
+    //     canvas_pos = scale * pos_circles;
+    //     // Fill in canvas at the canvas coords
+    //     c.write_pixel(Color(1, 0, 0), canvas_pos.x, canvas_pos.y);
+    //     // Rotate base position
+    //     pos_circles = one_of_10_rotations * pos_circles;
+    // };
