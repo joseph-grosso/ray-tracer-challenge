98dcd3005d02ae0ec38e12612527517c059d46ae
joegr <joe.grosso@cogeco.ca>
2024-10-14 21:18:32 -0400

    Remove grandient_pattern.cpp

--- src/patterns/conical_ring_pattern.h
+++ src/patterns/conical_ring_pattern.h
@@ -1,25 +1,31 @@
 #pragma once
 
+#include <cmath>
+
 #include "color.h"
 #include "matrix.h"
 #include "pattern.h"
 #include "tuple.h"
 
-#include <cmath>
-
 class ConicalRingPattern : public Pattern {
-private:
+ private:
   Color a;
   Color b;
 
-public:
+ public:
   ConicalRingPattern(Color a, Color b, Matrix t) : Pattern(t) {
     this->a = a;
     this->b = b;
   };
   ConicalRingPattern(Color a, Color b)
       : ConicalRingPattern(a, b, identity_matrix(4)){};
-  Color get_a();
-  Color get_b();
-  Color pattern_at(Tuple p);
+  Color get_a() { return a; };
+  Color get_b() { return b; };
+  Color pattern_at(Tuple p) {
+    return (int)std::floor(std::sqrt(std::pow(p.x, 2) + std::pow(p.z, 2))) %
+                       2 ==
+                   0
+               ? a
+               : b;
+  };
 };
