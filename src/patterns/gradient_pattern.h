fb58e8803101d7e89c05316587cb5274b6459f3f
joegr <joe.grosso@cogeco.ca>
2024-10-14 21:15:37 -0400

    Remove gradient_pattern.cpp

--- src/patterns/gradient_pattern.h
+++ src/patterns/gradient_pattern.h
@@ -1,25 +1,30 @@
 #pragma once
 
+#include <cmath>
+
 #include "color.h"
 #include "matrix.h"
 #include "pattern.h"
 #include "tuple.h"
 
-#include <cmath>
-
 class GradientPattern : public Pattern {
-private:
+ private:
   Color a;
   Color b;
 
-public:
+ public:
   GradientPattern(Color a, Color b, Matrix t) : Pattern(t) {
     this->a = a;
     this->b = b;
   };
   GradientPattern(Color a, Color b)
       : GradientPattern(a, b, identity_matrix(4)){};
-  Color get_a();
-  Color get_b();
-  Color pattern_at(Tuple p);
+  Color get_a() { return a; };
+  Color get_b() { return b; };
+  Color pattern_at(Tuple p) {
+    Color distance = b - a;
+    float fraction = p.x - floor(p.x);
+
+    return a + (distance * fraction);
+  };
 };
