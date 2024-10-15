c7ef6fccf8fa4213fc3367550047968dd43bdb61
joegr <joe.grosso@cogeco.ca>
2024-10-14 21:03:32 -0400

    Remove stripe_pattern.cpp and add all to header file

--- src/patterns/stripe_pattern.cpp
+++
@@ -1,9 +0,0 @@
-#include "stripe_pattern.h"
-
-Color StripePattern::get_a() { return a; };
-
-Color StripePattern::get_b() { return b; };
-
-Color StripePattern::pattern_at(Tuple p) {
-  return ((int)std::floor(p.x) % 2 == 0) ? a : b;
-};
--- src/patterns/stripe_pattern.h
+++ src/patterns/stripe_pattern.h
@@ -1,24 +1,24 @@
 #pragma once
 
+#include <cmath>
+
 #include "color.h"
 #include "matrix.h"
 #include "pattern.h"
 #include "tuple.h"
 
-#include <cmath>
-
 class StripePattern : public Pattern {
-private:
+ private:
   Color a;
   Color b;
 
-public:
+ public:
   StripePattern(Color a, Color b, Matrix t) : Pattern(t) {
     this->a = a;
     this->b = b;
   };
   StripePattern(Color a, Color b) : StripePattern(a, b, identity_matrix(4)){};
-  Color get_a();
-  Color get_b();
-  Color pattern_at(Tuple p); // must be a point, not vector
+  Color get_a() { return a; };
+  Color get_b() { return b; };
+  Color pattern_at(Tuple p) { return ((int)std::floor(p.x) % 2 == 0) ? a : b; };
 };
