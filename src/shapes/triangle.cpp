f9c00a3bc0ef6b542061b22e8b068d9cb75ea7dd
joegr <joe.grosso@cogeco.ca>
2025-01-25 00:26:46 -0500

    Add in local intersect code for the triangle

--- src/shapes/triangle.cpp
+++ src/shapes/triangle.cpp
@@ -5,9 +5,31 @@
 #include "intersection.hpp"
 
 Intersections Triangle::local_intersect(Ray r) {
-  std::vector<Intersection> xs = std::vector<Intersection>{};
+  // Moller-Trumbore algorithm
+  Tuple dir_cross_e2 = r.get_direction().cross(e2);
+  float det = e1.dot(dir_cross_e2);
 
-  return Intersections(xs);
+  if (equalByEpsilon(det, 0)) {
+    return Intersections();
+  };
+
+  float f = 1.0 / det;
+  Tuple p1_to_origin = r.get_origin() - p1;
+  float u = f * p1_to_origin.dot(dir_cross_e2);
+  if (u < 0 || u > 1) {
+    return Intersections();
+  };
+
+  Tuple origin_cross_e1 = p1_to_origin.cross(e1);
+  float v = f * r.get_direction().dot(origin_cross_e1);
+  if (v < 0 || (u + v) > 1) {
+    return Intersections();
+  };
+
+  float t = f * e2.dot(origin_cross_e1);
+
+  Intersection i = Intersection(t, this);
+  return Intersections(std::vector<Intersection>{i});
 };
 
 Tuple Triangle::local_normal_at(float x, float y, float z) { return normal; };
--- tests/ch15.1_triangle_tests.cpp
+++ tests/ch15.1_triangle_tests.cpp
@@ -39,3 +39,74 @@ TEST(TestTriangles, TriangleNormal) {
   EXPECT_EQ(t.normal, t.local_normal_at(point(-0.5, 0.75, 0)));
   EXPECT_EQ(t.normal, t.local_normal_at(point(0.5, 0.25, 0)));
 }
+
+// Scenario: Intersecting a ray parallel to the triangle
+// p210
+TEST(TestTriangles, TriangleParallelRay) {
+  Tuple p1 = point(0, 1, 0);
+  Tuple p2 = point(-1, 0, 0);
+  Tuple p3 = point(1, 0, 0);
+  Triangle t = Triangle(p1, p2, p3);
+  Ray r = Ray(point(0, -1, -2), vector(0, 1, 0));
+
+  Intersections xs = t.local_intersect(r);
+
+  EXPECT_EQ(xs.count, 0);
+}
+
+// Scenario: A ray misses the p1-p3 edge
+// p211
+TEST(TestTriangles, RayMissesTriangleEdge1) {
+  Tuple p1 = point(0, 1, 0);
+  Tuple p2 = point(-1, 0, 0);
+  Tuple p3 = point(1, 0, 0);
+  Triangle t = Triangle(p1, p2, p3);
+  Ray r = Ray(point(1, 1, -2), vector(0, 0, 1));
+
+  Intersections xs = t.local_intersect(r);
+
+  EXPECT_EQ(xs.count, 0);
+}
+
+// Scenario: A ray misses the p1-p2 edge
+// p211
+TEST(TestTriangles, RayMissesTriangleEdge2) {
+  Tuple p1 = point(0, 1, 0);
+  Tuple p2 = point(-1, 0, 0);
+  Tuple p3 = point(1, 0, 0);
+  Triangle t = Triangle(p1, p2, p3);
+  Ray r = Ray(point(-1, 1, -2), vector(0, 0, 1));
+
+  Intersections xs = t.local_intersect(r);
+
+  EXPECT_EQ(xs.count, 0);
+}
+
+// Scenario: A ray misses the p2-p3 edge
+// p211
+TEST(TestTriangles, RayMissesTriangleEdge3) {
+  Tuple p1 = point(0, 1, 0);
+  Tuple p2 = point(-1, 0, 0);
+  Tuple p3 = point(1, 0, 0);
+  Triangle t = Triangle(p1, p2, p3);
+  Ray r = Ray(point(0, -1, -2), vector(0, 0, 1));
+
+  Intersections xs = t.local_intersect(r);
+
+  EXPECT_EQ(xs.count, 0);
+}
+
+// Scenario: A ray strikes the triangle
+// p211
+TEST(TestTriangles, RayHitsTriangle) {
+  Tuple p1 = point(0, 1, 0);
+  Tuple p2 = point(-1, 0, 0);
+  Tuple p3 = point(1, 0, 0);
+  Triangle t = Triangle(p1, p2, p3);
+  Ray r = Ray(point(0, 0.5, -2), vector(0, 0, 1));
+
+  Intersections xs = t.local_intersect(r);
+
+  EXPECT_EQ(xs.count, 1);
+  EXPECT_TRUE(equalByEpsilon(xs[0].t, 2));
+}
